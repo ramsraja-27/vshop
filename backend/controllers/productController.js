cacad67451a2d8cf47f0ced18fcbd55f500f8008
VIJAY G <vjguru40@gmail.com>
2021-10-15 13:23:53 +0530

    Update productController.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,7 +3,7 @@ import Product from '../model/productModel.js';
 
 
 const getProducts=asyncHandler(async(req,res)=>{
-    const pageSize=12
+    const pageSize=8
     const page=Number(req.query.pageNumber) || 1
     const keyword=req.query.keyword ? {
         name:{
